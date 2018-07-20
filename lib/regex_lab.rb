2a103d4c3e14846125b1a2cc96cd6ef14d426018
Karen C <chenyanhuan66@163.com>
2018-07-20 03:04:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,13 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un+\w+ing/)
-
 
 end
 
 def words_five_letters_long(text)
-  text.grep(/\w{5}/)
+  text.scan(/\w{5}/)
 
 end
 
