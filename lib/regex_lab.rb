8e8f834439b9bb21111b13e1c588a27b3bfe44a4
Karen C <chenyanhuan66@163.com>
2018-07-20 02:45:58 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,15 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/[aeiou]+\w/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un+\w+ing/)
+  
 
 end
 
 def words_five_letters_long(text)
+  text.grep(/\w{5}/)
 
 end
 
@@ -15,5 +18,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+  word.scan(/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/)
 
 end
