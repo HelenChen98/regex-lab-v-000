a574ccf13c60a4ecc13c18361afb3385082134ce
Karen C <chenyanhuan66@163.com>
2018-07-20 03:02:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.scan(/[aeiou]+\w/)
+  if word[0].match(/[AEIOUaeiou]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
